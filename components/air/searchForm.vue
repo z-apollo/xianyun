296d3b0d02850093fba2134e7c1b3b63fcc317dd
z-apollo <54058143+z-apollo@users.noreply.github.com>
2019-10-14 19:24:50 +0800

    18.获取搜索建议城市

--- components/air/searchForm.vue
+++ components/air/searchForm.vue
@@ -91,14 +91,26 @@ export default {
         // cb:回调函数，必须要调用，调用时候必须要传递一个数组的参数，
         // 数组中的元素必须是一个对象，对象中必须要有value属性
         queryDepartSearch(value, cb){
+            //输入框为空时不请求
+            if(!value) return;
             
             // 请求搜索建议城市
-            
-            // var arr = [
-            //     {value: "广州"},
-            //     {value: "广元"},
-            // ]
-            cb(arr)
+            this.$axios({
+                url:"/airs/city?name=" + value
+            }).then(res =>{
+                // console.log(res)
+                //data是后台返回的城市数组，没有value属性
+                const {data} = res.data
+                //循环给每一项添加value属性
+                const newData = data.map(item =>{
+                    item.value = item.name.replace("市", ""); //去掉"市"字
+                    return item
+                })
+
+                //展示到下拉列表
+                cb(newData)
+            })
+
         },
         // 目标城市输入框获得焦点时触发
         // value 是选中的值，cb是回调函数，接收要展示的列表
