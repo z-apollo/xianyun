2ee8d09b3e4e3605f2cbd60b2f541b69643cbec5
z-apollo <54058143+z-apollo@users.noreply.github.com>
2019-10-15 15:07:50 +0800

    24.特价机票列表

--- pages/air/index.vue
+++ pages/air/index.vue
@@ -38,7 +38,21 @@
     </h2>
 
     <!-- 特价机票 -->
-    <div class="air-sale"></div>
+    <div class="air-sale">
+      <el-row type="flex" class="air-sale-pic" justify="space-between">
+        <el-col :span="6" v-for="(item, index) in sales" :key="index">
+          <nuxt-link
+            :to="`/air/flights?departCity=${item.departCity}&departCode=${item.departCode}&destCity=${item.destCity}&destCode=${item.destCode}&departDate=${item.departDate}`"
+          >
+            <img :src="item.cover" />
+            <el-row class="layer-bar" type="flex" justify="space-between">
+              <span>{{item.departCity}}-{{item.destCity}}</span>
+              <span>￥{{item.price}}</span>
+            </el-row>
+          </nuxt-link>
+        </el-col>
+      </el-row>
+    </div>
   </section>
 </template>
 
@@ -46,10 +60,24 @@
 import SearchForm from "@/components/air/searchForm";
 
 export default {
+  data(){
+    return {
+      sales: []
+    }
+  },
   components: {
     SearchForm
+  },
+  mounted(){
+    this.$axios({
+      url:"/airs/sale",
+    }).then(res =>{
+      const {data} = res.data
+
+      this.sales = data
+    })
   }
-};
+}
 </script>
 
 <style scoped lang="less">
